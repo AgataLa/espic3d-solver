4cff1a0c3a1fccd3f7354d5567fc6bfa756f473e
Agata Lachowiecka <64068771+AgataLa@users.noreply.github.com>
2024-06-21 00:20:50 +0200

    Update tsi-measure.jl

--- tsi-measure.jl
+++ tsi-measure.jl
@@ -1,8 +1,8 @@
 include("src/pic3d.jl")
 using .PIC3D
-using PyCall
-using PyPlot
 using LinearAlgebra
+
+filename = "results.txt"
 
 function generate_particles!(NP, d)
     num = d / (2*NP / (PIC3D.XL*PIC3D.YL*PIC3D.ZL))
@@ -28,65 +28,38 @@ function generate_particles!(NP, d)
     return electrons, ions
 end
 
-function compute_EK_particles(electrons, ions)
-    ek = 0
-    @inbounds for i in eachindex(electrons.v)
-        ek += electrons.m*dot(electrons.v[i], electrons.v[i])
-    end
-    @inbounds for i in eachindex(ions.v)
-        ek += ions.m*dot(ions.v[i], ions.v[i])
-    end
-    ek = 0.5*ek
-    return ek
-end
 
-
-function compute_EP_field()
-    ep = 0
-    @inbounds for k = 1:(PIC3D.NZ-1), j = 1:(PIC3D.NY-1), i = 1:(PIC3D.NX-1)
-        ep += PIC3D.Ex[i,j,k]^2 + PIC3D.Ey[i,j,k]^2 + PIC3D.Ez[i,j,k]^2
-    end
-    ep = 0.5 * PIC3D.ε_0 * ep * PIC3D.Δx*PIC3D.Δy*PIC3D.Δz
-    return ep
-end
-
-NP = 2048 #2048
+NP = 2048
 d = 4.5e9
 electrons, ions = generate_particles!(NP, d)
 
 Δt = 1e-9
-timesteps = 3000
+timesteps = 1000
 
-historyxx = []
-historyvel = []
-EK = []
-EK2 = []
-EE = []
-EP = []
-
+t1 = time();
 for i = 1:timesteps
-    println(i)
     PIC3D.timestep_FFT!(1.0, electrons, ions)
-    if i % 500 == 0
-        plt.figure()
-        ax = plt.gca()
-    ax.set_ylim([-1e6,1e6])
-        plt.scatter([electrons.x[i][1] for i in eachindex(electrons.x) if i % 2 == 0],
-                    [electrons.v[i][1] for i in eachindex(electrons.v) if i % 2 == 0], label="c = 1", s=2)
-        plt.scatter([electrons.x[i][1] for i in eachindex(electrons.x) if i % 2 == 1],
-                    [electrons.v[i][1] for i in eachindex(electrons.v) if i % 2 == 1], label="c = 1", s=2)
-        plt.xlabel("x [m]")
-        plt.ylabel("vₓ [m/s]")
-        plt.tight_layout()
-        ax.set_ylim([-1e6,1e6])
-        #plt.title("$(i) ns symulacji")
-        #plt.show()
-        plt.savefig("FFT-$(i).png")
-        push!(historyxx, [[ex...] for ex in electrons.x])
-        push!(historyvel, [[ev...] for ev in electrons.v])
+    if i % 100 == 0
+        elapsed_time = time() - t1
+        open(filename, "w") do file
+            write(file, "FFT: $(i)      $(elapsed_time)\n")
+        end
+        t1 = time();
     end
-    push!(EK, compute_EK_particles(electrons, ions))
-    push!(EK2, compute_EK_particles2(electrons, ions, NP, d))
-    push!(EE, compute_EP_field())
-    push!(EP, compute_EP_potential())
 end
+
+
+# include("src/pic3d.jl")
+# using .PIC3D
+# electrons, ions = generate_particles!(NP, d)
+# for i = 1:timesteps
+#     PIC3D.timestep!(1.0, electrons, ions)
+# end
+
+
+# include("src/pic3d.jl")
+# using .PIC3D
+# electrons, ions = generate_particles!(NP, d)
+# for i = 1:timesteps
+#     PIC3D.timestep_multigrid!(1.0, electrons, ions)
+# end
